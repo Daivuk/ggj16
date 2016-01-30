e1df520edad6ba9cc52c817c28f3df62f5d8f260
Daivuk <daivuk@gmail.com>
2016-01-29 21:14:28 -0500

    Fireplace anim

--- src/app/Fireplace.cpp
+++ src/app/Fireplace.cpp
@@ -1,7 +1,12 @@
 #include "Fireplace.h"
+#include "Globals.h"
 
 Fireplace::Fireplace(seed::View* pView, const Vector2& position)
 {
+    auto pFireAnim = pView->CreateSpriteWithSpriteAnim("fireplace.spriteanim", "fire1");
+    pFireAnim->SetPosition(position);
+    pFireAnim->SetScale(Vector2(SPRITE_SCALE));
+    pView->AddNode(pFireAnim);
 }
 
 Fireplace::~Fireplace()
--- src/app/Globals.h
+++ src/app/Globals.h
@@ -4,3 +4,5 @@
 class Player;
 
 typedef std::vector<Player*>     PlayerVect;
+
+#define SPRITE_SCALE (1.f / 16.f)
